dcf903ede0708804ebe432fcf55dc96c0b016063
Tamanna Thakur <160922262+tamannathakur1@users.noreply.github.com>
2025-07-10 13:23:16 +0530

    Add files via upload

--- movie_app.py
+++ movie_app.py
@@ -12,4 +12,13 @@ PLATFORM_LOGOS = {
     "netflix": ("Netflix", "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"),
     "prime": ("Amazon Prime Video", "https://upload.wikimedia.org/wikipedia/commons/f/f1/Prime_Video.png"),
     "disney": ("Disney+", "https://upload.wikimedia.org/wikipedia/commons/3/3e/Disney%2B_logo.svg"),
-    "hotstar": ("Hotstar
+    "hotstar": ("Hotstar", "https://upload.wikimedia.org/wikipedia/commons/1/1e/Hotstar_logo.svg"),
+    "hbo": ("HBO Max", "https://upload.wikimedia.org/wikipedia/commons/1/17/HBO_Max_Logo.svg"),
+    "apple": ("Apple TV+", "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg"),
+    "zee5": ("Zee5", "https://upload.wikimedia.org/wikipedia/commons/6/67/ZEE5_official_logo.png"),
+    "sonyliv": ("SonyLIV", "https://upload.wikimedia.org/wikipedia/commons/b/b2/Sony_Liv_logo.svg"),
+    "aha": ("Aha", "https://upload.wikimedia.org/wikipedia/commons/f/f4/Aha_OTT_Logo.png"),
+    "jio": ("JioCinema", "https://upload.wikimedia.org/wikipedia/commons/0/0c/Jio_Cinema_Logo.png")
+}
+
+   
